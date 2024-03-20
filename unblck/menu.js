21fdab2d7932810ec1ac36b9da281b99e92e46f9
Ingo Wolf <me@ingowolf.au>
2024-03-20 20:59:49 +1100

    add menu hide on click outside and change z-index so its not 9999

--- unblck/menu.js
+++ unblck/menu.js
@@ -2,3 +2,7 @@ function menu(menuName) {
   document.querySelector(".menucontainer").classList.remove("hide");
   //   document.querySelector(".menu").innerHTML = menu[menuName];
 }
+
+function closeMenu() {
+  document.querySelector(".menucontainer").classList.add("hide");
+}
