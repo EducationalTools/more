b206a0fcc64d7b5b9c39a2d5d0a96a80ffc6217b
Ingo Wolf <me@ingowolf.au>
2024-03-20 21:06:58 +1100

    clear menu content

--- unblck/menu.js
+++ unblck/menu.js
@@ -1,5 +1,6 @@
 function menu(menuName) {
   document.querySelector(".menucontainer").classList.remove("hide");
+  document.querySelector(".menucontainer > .menu").innerHTML = "";
   //   document.querySelector(".menu").innerHTML = menu[menuName];
 }
 
