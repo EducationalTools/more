e46960681b7cfe40113a636660d4ccd4416a7cc4
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-03-27 10:09:17 +0000

    added gmae menu, closes #21

--- u/menu.js
+++ u/menu.js
@@ -17,6 +17,13 @@ function menu(menuName) {
     header = document.createElement("h1");
     header.innerHTML = "Gmaes";
     document.querySelector(".menucontainer > .menu").appendChild(header);
+    document.config.games.forEach(game => {
+      menuitm = document.createElement("a");
+      menuitm.classList.add("menuitm");
+      menuitm.href = game.repo;
+      menuitm.innerHTML = game.name;
+      document.querySelector(".menucontainer > .menu").appendChild(menuitm);
+    });
   }
   if (menuName == "app") {
     header = document.createElement("h1");
